76fc4cc233699064d41d1713476bb5fe6decb989
Saad Subhani <saad.subhani123456@gmail.com>
2022-11-22 13:48:31 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BMI_Cal';
+  title = 'BMI Calculator';
+  
+  weight!: number;
+  height!: number;
+  result!: number;
+  answer!: string;
+
+
+  calcBMI(){
+    const kg = this.weight*0.453592;
+    const meters= this.height*0.0254;
+    this.result=kg / (meters * meters)
+    this.generate_answer();
+  }
+  generate_answer(){
+    if(this.result<18.5){
+      this.answer="You are Underweight";
+    }
+    else if(this.result>=18.5 && this.result<=24.9){
+      this.answer="Congrats! You have a Normal BMI";
+    }
+    else if(this.result>=25 && this.result<=29.9){
+      this.answer="You are Overweight";
+    }
+    else if(this.result>=30){
+      this.answer="Warning! You are Obese";
+    }
+  }
+  
+
 }
