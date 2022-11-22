f5ba20c960d9abbfe173c953dc5a90547874ddde
Saad Subhani <saad.subhani123456@gmail.com>
2022-11-22 16:35:38 +0500

    Checks added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,13 @@ export class AppComponent {
 
 
   calcBMI(){
-    this.calculation();
+    if(this.weight!=null && this.height!=null){
+        this.calculation();
   }
+  else{
+    alert("Please enter a valid weight and height");
+  }
+}
   calculation(){
     const kg = this.weight*0.453592;
     const meters= this.height*0.0254;
@@ -24,8 +29,14 @@ export class AppComponent {
     this.generate_answer();
   }
   generate_answer(){
-    if(this.result<18.5){
+    if(this.result<0){
+      alert("Please enter a valid weight and height");
+      this.result=0;
+      this.answer="";
+    }
+    else if(this.result<18.5){
       this.answer="You are Underweight";
+      
     }
     else if(this.result>=18.5 && this.result<=24.9){
       this.answer="Congrats! You have a Normal BMI";
